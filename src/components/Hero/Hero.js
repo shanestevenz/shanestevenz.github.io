d36f182e561a8819a56e0cd29891764d518d0feb
Shrawr <Shrawr@users.noreply.github.com>
2022-04-18 15:17:12 -0400

    added header, projects, etc

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,23 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding> 
+<LeftSection>
+  <SectionTitle main center>
+    Welcome to <br/>
+      my Portfolio
+  </SectionTitle>
+
+  <SectionText>
+    The purpose of this website was to learn more about React and Next.js
+  </SectionText>
+  <Button onClick = {() => window.location = 'https://google.com'}  > 
+    Learn More
+  </Button>
+</LeftSection>
+
+  </Section>
 );
 
 export default Hero;
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,9 +5,37 @@ import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalCompon
 import { projects } from '../../constants/constants';
 
 const Projects = () => (
-  <div>
-    Projects
-  </div>
+  <Section nopadding id='projects'>
+    < SectionDivider />
+    <SectionTitle main>Projets</SectionTitle>
+    <GridContainer>
+      {projects.map((project) => (
+        <BlogCard>
+          <Img src={project.image} />
+          <TitleContent>
+            <HeaderThree title> {project.title}</HeaderThree>
+            <Hr />
+
+          </TitleContent>
+          <CardInfo> {project.description} </CardInfo>
+
+          <div>
+            <TitleContent>Tools/Skills</TitleContent>
+            <TagList>
+
+              {project.tags.map((tag, i) => (
+                <Tag key={i}>{tag} </Tag>
+              ))}
+            </TagList>
+          </div>
+          <UtilityList>
+            <ExternalLinks href={project.visit}> Code </ExternalLinks>
+            <ExternalLinks href={project.source}> Source </ExternalLinks>
+          </UtilityList>
+        </BlogCard>
+      ))}
+    </GridContainer>
+  </Section>
 );
 
 export default Projects;
